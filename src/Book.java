43abec66438cf33c767fd33a3ab66f9e7da2f234
Imre Mathias <imremathias@outlook.com>
2021-02-10 14:47:44 +0100

    Finished program initial sout's + Book-class with methods

--- src/Book.java
+++ src/Book.java
@@ -4,4 +4,56 @@ public class Book {
     private String authorName;
     private short pageCount;
     private Genre genre;
+
+    public void Book(long ISBN, String bookTitle, String authorName, short pageCount, Genre genre) {
+        this.ISBN = ISBN;
+        this.bookTitle = bookTitle;
+        this.authorName = authorName;
+        this.pageCount = pageCount;
+        this.genre = genre;
+    }
+
+    // START -- GETTERS & SETTERS
+
+        public long getISBN() {
+            return ISBN;
+        }
+
+        public void setISBN(long ISBN) {
+            this.ISBN = ISBN;
+        }
+
+        public String getBookTitle() {
+            return bookTitle;
+        }
+
+        public void setBookTitle(String bookTitle) {
+            this.bookTitle = bookTitle;
+        }
+
+        public String getAuthorName() {
+            return authorName;
+        }
+
+        public void setAuthorName(String authorName) {
+            this.authorName = authorName;
+        }
+
+        public short getPageCount() {
+            return pageCount;
+        }
+
+        public void setPageCount(short pageCount) {
+            this.pageCount = pageCount;
+        }
+
+        public Genre getGenre() {
+            return genre;
+        }
+
+        public void setGenre(Genre genre) {
+            this.genre = genre;
+        }
+
+    // END -- GETTERS & SETTERS
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        // Read data from bok.txt and add book objects to ArrayList
+        // Read data from bok.txt and add book objects from those to ArrayList
+
+        System.out.println("Welcome to the library!" + "\n");
+        System.out.println("Press 1: Get overview of books currently in the library");
+        System.out.println("Press 2: Add book to the library");
+        System.out.println("Press 3: Change book metadata");
+        System.out.println("Press 4: Find books in specified genre");
+        System.out.println("Press 5: Find books from their author");
+        System.out.println("Press 6: Find books from their ISBN");
+        System.out.println("Press 7: Remove book");
     }
 }
